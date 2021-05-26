4f7b9cbb8a65a7f6c97cf5a213c0d8e0d94ae78d
gongenryo <gongenta0519@gmail.com>
2021-05-26 10:54:57 +0900

    edit.blade.phpの作成

---
+++ birthday/resources/views/birthday/edit.blade.php
@@ -0,0 +1,11 @@
+<form method="POST" action="{{ route('birthday.update', ['id' => $friend->id]) }}">
+@csrf
+名前
+<input type="text" name="name" value="{{ $friend->name }}">
+<br>
+誕生日
+<input type="datetime-local" name="birthday" value="{{ $friend->birthday }}">
+
+<input class="btn btn-primary" type="submit" value="更新する">
+</form>
+
