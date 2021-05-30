1304122d75ee612d1267c4edefc42b9d518d3cff
gongenryo <gongenta0519@gmail.com>
2021-05-31 07:36:45 +0900

    前回のコミットの不足分の補足

--- birthday/resources/views/birthday/index.blade.php
+++ birthday/resources/views/birthday/index.blade.php
@@ -1,6 +1,7 @@
-
-<?php
-echo "<pre>";
-var_dump($data);
-echo "</pre>";
-?>
+@foreach($index_lists as $index_list)
+  {{ $index_list->name }}
+  {{ $index_list->birthday }}
+  <a href="{{ route('birthday.show', ['id' => $index_list->id]) }}">詳細を見る</a>
+  <br>
+@endforeach
+<a href="{{ route('birthday.create') }}">新規作成</a>
